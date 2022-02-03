f358a6da87ce5da7949e2bc589a5d4909c0629a6
MayurAnklekar <mayuranklekar1010@gmail.com>
2022-02-03 19:39:03 +0530

    finished the build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -10,7 +17,7 @@ export default function Home() {
       </Head>
 
       <Header></Header>
-      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-6'>
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-6">
         <div className="px-10 space-y-5">
           <h1 className="text-6xl font-serif max-w-xl">
             <span className="underline decoration-black decoration-4">
@@ -29,6 +36,57 @@ export default function Home() {
           alt=""
         />
       </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-3 md:p-6">
+        {posts.map((post) => {
+          return (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="border rounded-lg overflow-hidden group cursor-pointer">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    slug,
+    mainImage,
+    author->{
+    name,
+    image,
+    
+  },
+  description
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
